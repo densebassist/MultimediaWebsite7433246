c24222be8db9f7cef99fba1183a577517960ff7b
densebassist <owen.gregory@education.nsw.gov.au>
2026-08-19 17:51:45 +1000

    added back button and fixed door glitch

--- scripts/buttons.gd
+++ scripts/buttons.gd
@@ -1,5 +1,5 @@
 extends Control
-
+@export_file("*.tscn") var last_scene: String
 
 
 
@@ -8,11 +8,24 @@ func _on_button_quit_pressed() -> void:
 	await ScreenTransition.get_node("AnimationPlayer").animation_finished
 	get_tree().quit()
 	
+	
+func _on_button_back_pressed() -> void:
+	if last_scene == "":
+		print("ERROR: No previous scene assigned!")
+		return
+	
+	print("Going back to: ", last_scene)
+	
+	ScreenTransition.get_node("AnimationPlayer").play("fade_to_black")
+	await ScreenTransition.get_node("AnimationPlayer").animation_finished
+	
+	get_tree().change_scene_to_file(last_scene)
+	ScreenTransition.get_node("AnimationPlayer").play("fade_in")
 
 
 func _on_button_home_pressed() -> void:
 	ScreenTransition.get_node("AnimationPlayer").play("fade_to_black")
-	await ScreenTransition.get_node("AnimationPlayer").animation_finished
+	#await ScreenTransition.get_node("AnimationPlayer").animation_finished
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 	ScreenTransition.get_node("AnimationPlayer").play("fade_in")
 	
--- scripts/door.gd
+++ scripts/door.gd
@@ -1,51 +1,56 @@
 extends Area2D
-func _ready() -> void:
-	print("test")
 
 # Tracks if the player is standing on the teleporter
 var is_player_touching: bool = false
+
+# Prevents the teleporter from being triggered multiple times
+var teleporting: bool = false
 
 # The target coordinates where the player will be sent
 @export var target_position: Vector2 = Vector2(-720, 584)
 @export var target_scene: PackedScene
 
 
+func _ready() -> void:
+	print("test")
+
+
 func _on_body_entered(body: Node2D) -> void:
-	
 	print("Inside")
+	
 	if body.is_in_group("Player"):
 		is_player_touching = true
-		
+
 
 func _on_body_exited(body: Node2D) -> void:
-	print("Outisde")
+	print("Outside")
+	
 	if body.is_in_group("Player"):
 		is_player_touching = false
 
+
 func _unhandled_input(event: InputEvent) -> void:
-	# Checks if the player is touching the object AND presses E
-	if is_player_touching and Input.is_action_pressed("interact"):
-		await get_tree().create_timer(0.2).timeout
-		print("Teleporting")
-		ScreenTransition.get_node("AnimationPlayer").play("fade_to_black")
-		await ScreenTransition.get_node("AnimationPlayer").animation_finished
+	# Only trigger once when E is initially pressed
+	if event.is_action_pressed("interact") and is_player_touching and not teleporting:
 		teleport_player()
-		
+
 
 func teleport_player() -> void:
-	if target_scene:
-		await get_tree().create_timer(1).timeout
-		if not target_scene:
-			print("Warning: No target scene assigned to this teleporter!")
-			
-		else:
-			get_tree().change_scene_to_packed(target_scene)
-	else:
+	if not target_scene:
 		print("Warning: No target scene assigned to this teleporter!")
-
+		return
 	
+	teleporting = true
 	
-		#player.global_position = target_position
-		#await get_tree().create_timer(1.0).timeout
-
+	print("Teleporting")
+	
+	# Fade to black
+	ScreenTransition.get_node("AnimationPlayer").play("fade_to_black")
+	await ScreenTransition.get_node("AnimationPlayer").animation_finished
+	
+	# Small delay before changing scenes
+	await get_tree().create_timer(1.0).timeout
+	
+	# Change scene
+	get_tree().change_scene_to_packed(target_scene)
 	ScreenTransition.get_node("AnimationPlayer").play("fade_in")
